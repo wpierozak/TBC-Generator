7be6a7dd0476a7edc8be396f357275b595fd4770
Wiktor Pierozak <pierozakwiktor@gmail.com>
2024-11-19 15:39:26 +0100

    Createad install option in cmake

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,18 @@ int main(int argc, const char** argv)
     ConfigurationParser parser;
     Configuration config;
 
+    if(argc != 2){
+        std::cout<< "Please provide path to the configuration file" << std::endl;
+        return -1;
+    }
+
     std::string inputFile = argv[1];
     try
     {
-    
         parser.parseXmlFile(inputFile);
         config = parser.createConfiguration();
-        //config.print();
         GenerationManager manager(config);
-        // Start time measurement for `manager.generate()`
-        auto start = std::chrono::high_resolution_clock::now();
-
         manager.generate();
-
-        // End time measurement
-        auto end = std::chrono::high_resolution_clock::now();
-
-        // Calculate the elapsed time in milliseconds
-        std::chrono::duration<double, std::milli> elapsed = end - start;
-
-        // Print out the elapsed time
-        std::cout << "Generation time: " << elapsed.count() << " ms" << std::endl;
 
         saveMicrostructureFile(manager.domain(),config);
     }
